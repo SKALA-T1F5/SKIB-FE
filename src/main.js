c3e1cba72077e5fa704347a119ebe95b89ab1a71
hgit600 <hunique8832@naver.com>
2025-06-17 09:14:33 +0900

    fix:main.js and index

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js' // .js 확장자 명시 (필수는 아니지만 명확성을 위해)
+import './style.scss'; //trainer
+// import './assets/main.css'; //trainee
+
+
+import vuetify from './plugins/vuetify'; //trainer
+import { PerfectScrollbar } from 'vue3-perfect-scrollbar'; //trainer
+import 'vue3-perfect-scrollbar/style.css'; //trainer
+import VueApexCharts from 'vue3-apexcharts'; //trainer
+import '@mdi/font/css/materialdesignicons.css'; //trainer
+
+
 
 const app = createApp(App)
 
 app.use(router)
-
-app.mount('#app')
+    .use(vuetify) //trainer
+    .use(VueApexCharts) //trainer
+    .component('PerfectScrollbar', PerfectScrollbar) //trainer
+    .mount('#app');
