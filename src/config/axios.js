2310887713e83e5389c524b7cab1216497d721cb
Laon0821 <sb723071@gmail.com>
2025-06-17 17:38:48 +0900

    feat: axios 기반 Springboot 연동 준비

---
+++ src/config/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+// axios Instance 생성
+const api = axios.create({
+  baseURL: 'https://skib-backend.skala25a.project.skala-ai.com/api',
+  headers: {
+    accept: 'application/json',
+  },
+})
+
+// Token을 요청 Header에 추가
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// 세션 만료 안내
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      alert('세션이 만료되었습니다. 다시 로그인해주세요.')
+      localStorage.removeItem('token')
+      localStorage.removeItem('role')
+      window.location.href = '/'
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default api
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +13,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      '/api': {
+        target: 'https://skib-backend.skala25a.project.skala-ai.com/', // SpringBoot 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 })
