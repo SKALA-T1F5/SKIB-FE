fed22ae19f1ee01b07d47c242a1903a1c2263e4e
Laon0821 <sb723071@gmail.com>
2025-06-26 17:09:37 +0900

    feat: TrainerTestManagement 중 TestPrompt 화면 UI 수정

--- src/components/trainer/test/TestPrompt.vue
+++ src/components/trainer/test/TestPrompt.vue
@@ -224,7 +224,7 @@ const sendPromptAndProceed = async () => {
   border: 1px solid #e0e0e0;
   border-radius: 8px;
   padding: 16px;
-  margin-bottom: 24px;
+  margin-bottom: 0;
 }
 
 .example-prompt-card {
