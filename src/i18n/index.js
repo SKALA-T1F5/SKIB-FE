404d0fcc6df6b431839e9de7aaa059a281e6bfcd
hgit600 <hunique8832@naver.com>
2025-07-02 17:23:33 +0900

    fix: translate TraineeMain.vue

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,22 +2,26 @@ import { createI18n } from 'vue-i18n'
 
 const messages = {
   ko: {
-    greeting: '안녕하세요! 궁금한 점이 있으면 언제든 질문해 주세요 :)',
-    submit: '제출',
-    prev: '이전 문제',
-    next: '다음 문제',
-    exit: '나가기',
-    welcome: '{userName} 매니저님, 반갑습니다!',
-    // ...추가 번역
+    main_welcome: '{userName} 매니저님, 반갑습니다!',
+    main_alert: '해당 조건에 일치하는 테스트가 없습니다.',
+    main_addTest: '+ 테스트 추가',
+    main_searchPlaceHolder: '테스트 이름을 검색하세요...',
+
+    addTestDesc: '테스트 초대 링크를 입력하고 언어를 선택하세요.',
+    inviteLink: '초대 링크',
+    selectLanguage: '언어 선택',
+
   },
   en: {
-    greeting: 'Hello! Feel free to ask any questions :)',
-    submit: 'Submit',
-    prev: 'Previous',
-    next: 'Next',
-    exit: 'Exit',
-    welcome: 'Welcome, Manager {userName}!',
-    // ...more translations
+    main_welcome: 'Welcome, Manager {userName}!',
+    main_alert: 'No test exist.',
+    main_addTest: '+ Add Test',
+
+
+    addTestDesc: 'Enter the test invitation link and select a language.',
+    inviteLink: 'Invitation Link',
+    selectLanguage: 'Select Language',
+
   },
 }
 
