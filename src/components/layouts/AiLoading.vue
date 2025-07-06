0aba8be96b32831b501539811eff7a0c805ae1a3
Laon0821 <sb723071@gmail.com>
2025-07-06 19:17:26 +0900

    style: AI Loading 활성화 시 배경색 죽이기

--- src/components/layouts/AiLoading.vue
+++ src/components/layouts/AiLoading.vue
@@ -31,6 +31,15 @@ defineProps({
 
 <style scoped>
 .ai-loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* 현재 배경색과 투명도 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
   z-index: 9999 !important;
 }
 
